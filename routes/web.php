ce55ac05acc975a7ee72885e826aef7c7ea8cdb3
Dimasadivia <Dimas020417@gmail.com>
2024-09-19 22:44:44 +0700

    routes-view

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------s-------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', [ProfileController::class, 'profile']);
+Route::get('/profile/{nama}/{kelas}/{npm}',
+[ProfileController::class, 'profile']);
